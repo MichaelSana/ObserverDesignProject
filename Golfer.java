8504ffab050777a5db1411bf65905f738dfab1c4
Michael Sana <msana@email.sc.edu>
2021-01-21 12:21:17 -0500

    implemented Subject interface and Golfer class

--- Golfer.java
+++ Golfer.java
@@ -1,3 +1,31 @@
-public class Golfer {
+import java.util.ArrayList;
+
+public class Golfer implements Subject {
+
+    ArrayList<Observer> observers;
+    private String name;
+    Golfer(String name){
+        this.name = name;
+    }
+
+    @Override
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObserver(int strokes, int par) {
+        
+
+    }
+
+    String getName() {
+        return this.name;
+    }
     
 }
--- Subject.java
+++ Subject.java
@@ -1,3 +1,5 @@
 public interface Subject{
-    
+    public void registerObserver(Observer observer);
+    public void removeObserver(Observer observer);
+    public void notifyObserver(int strokes, int par);
 }
