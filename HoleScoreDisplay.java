45094f0537705ff236059dc7db6bd4853b524e09
Michael Sana <msana@email.sc.edu>
2021-01-23 10:15:02 -0500

    Did HoleScoreDisplay file

--- HoleScoreDisplay.java
+++ HoleScoreDisplay.java
@@ -1,20 +1,34 @@
+import javax.lang.model.util.ElementScanner6;
+
 public class HoleScoreDisplay implements Observer {
     private Subject golfer;
     private int strokes;
     private int par;
 
     public HoleScoreDisplay(Subject golfer){
-
+        this.golfer = golfer;
+        golfer.registerObserver(this);
     }
 
     @Override
     public void update(int strokes, int par) {
-        // TODO Auto-generated method stub
-
+        this.strokes = strokes;
+        this.par = par;
+        displayCurrentScore();
     }
 
     private void displayCurrentScore(){
-
+        System.out.println("Current Hole Stats:");
+        int aPar = strokes - par;
+        if(strokes < par){
+            System.out.println(aPar +"under par");
+        }
+        else if (strokes > par) {
+            System.out.println(aPar + "over par");
+        }
+        else{
+            System.out.println("Made par");
+        }
     }
 
 
